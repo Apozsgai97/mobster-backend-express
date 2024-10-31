189c02cd768c38514c8ec60044485e40c53b9197
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-10-31 13:36:06 +0100

    add start to script

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
-import app from "./app";
+
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("express on vercel");
+});
+
+export default app;
 
 app.listen(3000, () => {
   console.log("server listening on http://localhost:3000/");
