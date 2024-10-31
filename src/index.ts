98c15ee8a623417330a5334c46b8a2296c8b058c
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-10-31 13:42:59 +0100

    delete vercel

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,11 @@ export default app;
 app.listen(3000, () => {
   console.log("server listening on http://localhost:3000/");
 });
+
+
+/*`GET YOUR_BACKEND_URL/mobs/` # get all mobs
+`POST YOUR_BACKEND_URL/mobs/` # add a new mob
+`GET YOUR_BACKEND_URL/mobs/:mobId` # get a particular mob
+`GET YOUR_BACKEND_URL/mobs/:mobId/members` # get all mob members of a particular mob
+`POST YOUR_BACKEND_URL/mobs/:mobId/members` # add a new mob-member to the mob
+`GET YOUR_BACKEND_URL/mobs/:mobId/members/:memberId` # get a particular mob-member of a particular mob*/
